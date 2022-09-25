dd7a61e1f256977e58d633592273206a7580fbae
kimdoyun98 <rladlsxo1250@gmail.com>
2022-09-25 20:16:04 +0900

    CycleFragment 요일 표시

--- app/src/main/java/com/example/todo_list/Adapter/CycleAdapter.kt
+++ app/src/main/java/com/example/todo_list/Adapter/CycleAdapter.kt
@@ -2,16 +2,18 @@ package com.example.todo_list.Adapter
 
 import android.app.AlertDialog
 import android.content.Context
-import android.content.DialogInterface
-import android.content.Intent
+import android.graphics.Color
+import android.text.SpannableString
+import android.text.Spanned
+import android.text.style.ForegroundColorSpan
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
-import com.example.todo_list.Activity.DetailActivity
 import com.example.todo_list.CycleViewModel
 import com.example.todo_list.data.CycleEntity
 import com.example.todo_list.databinding.RecyclerviewCycleItemBinding
+
 
 class CycleAdapter (val context: Context, val viewModel: CycleViewModel) : RecyclerView.Adapter<CycleAdapter.MyViewHolder>() {
     private lateinit var binding : RecyclerviewCycleItemBinding
@@ -44,6 +46,43 @@ class CycleAdapter (val context: Context, val viewModel: CycleViewModel) : Recyc
     override fun onBindViewHolder(holder: MyViewHolder, position: Int) {
         val data: CycleEntity = list[position]
         holder.bind(data)
+
+        /**
+         * 선택한 요일 색 표시
+         */
+        val content = " 월 화 수 목 금 토 일"
+        val spannableString = SpannableString(content)
+
+        var saveIndex = ArrayList<Int>()
+        for ( i: Int in 0..6){
+            if(data.day?.get(i) == true){
+                saveIndex.add(i)
+            }
+        }
+
+        if (saveIndex.size == 7) {
+            binding.showDay.text = "매일"
+            binding.showDay.setTextColor(Color.BLUE)
+        }
+        else{
+            for(i:Int in saveIndex){
+                var index : Int = 0
+                when(i){
+                    0 -> index = 0
+                    1 -> index = 2
+                    2 -> index = 4
+                    3 -> index = 6
+                    4 -> index = 8
+                    5 -> index = 10
+                    6 -> index = 12
+                }
+
+                val colorSpan = ForegroundColorSpan(Color.BLUE)
+                spannableString.setSpan(colorSpan, index-1, index, Spanned.SPAN_EXCLUSIVE_EXCLUSIVE)
+                binding.showDay.text = spannableString
+            }
+        }
+
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MyViewHolder {
