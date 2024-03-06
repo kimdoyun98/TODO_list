6e27f708a844c362c67c252e8a26d600c6cb8045
kimdoyun98 <rladlsxo1250@gmail.com>
2024-03-06 18:39:28 +0900

    알림 요청 위치 변경
    설정에서 알림 허용 했을 때 확인하여 사용자에게 알리는게 더 좋다고 판단

--- app/src/main/java/com/example/todo_list/fragment/SettingFragment.kt
+++ app/src/main/java/com/example/todo_list/fragment/SettingFragment.kt
@@ -1,13 +1,19 @@
 package com.example.todo_list.fragment
 
+import android.Manifest
 import android.content.SharedPreferences
+import android.os.Build
 import android.os.Bundle
 import android.util.Log
+import android.widget.Toast
+import androidx.annotation.RequiresApi
 import androidx.preference.Preference
 import androidx.preference.PreferenceFragmentCompat
 import androidx.preference.PreferenceManager
 import com.example.todo_list.R
 import com.example.todo_list.common.MyApplication
+import com.gun0912.tedpermission.PermissionListener
+import com.gun0912.tedpermission.normal.TedPermission
 
 class SettingFragment : PreferenceFragmentCompat(){
     lateinit var prefs : SharedPreferences
@@ -26,22 +32,40 @@ class SettingFragment : PreferenceFragmentCompat(){
         }
     }
 
+    @RequiresApi(Build.VERSION_CODES.TIRAMISU)
     private val prefListener = SharedPreferences.OnSharedPreferenceChangeListener { _, key ->
         when(key){
             "pushAlert" -> {
                 if(MyApplication.prefs.getAlarm("pushAlarm")) MyApplication.prefs.setAlarm("pushAlarm", false)
-                else MyApplication.prefs.setAlarm("pushAlarm", true)
+                else {
+                    TedPermission.create()
+                        .setPermissionListener(permission)
+                        .setDeniedMessage("권한이 거부되었습니다. 설정 > 권한에서 허용해주세요.")
+                        .setPermissions(Manifest.permission.POST_NOTIFICATIONS)
+                        .check()
+                    MyApplication.prefs.setAlarm("pushAlarm", true)
+                }
             }
         }
     }
 
+    @RequiresApi(Build.VERSION_CODES.TIRAMISU)
     override fun onResume() {
         super.onResume()
         prefs.registerOnSharedPreferenceChangeListener(prefListener)
     }
 
+    @RequiresApi(Build.VERSION_CODES.TIRAMISU)
     override fun onPause() {
         super.onPause()
         prefs.unregisterOnSharedPreferenceChangeListener(prefListener)
+    }
+
+    private val permission = object : PermissionListener {
+        override fun onPermissionGranted() {}
+
+        override fun onPermissionDenied(deniedPermissions: MutableList<String>?) {
+            Toast.makeText(MyApplication.instance.applicationContext, "권한 거부", Toast.LENGTH_SHORT).show()
+        }
     }
 }
