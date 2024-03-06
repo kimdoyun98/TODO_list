7d6c248fe11037fcefa97b034fb75292d7fc3c51
kimdoyun98 <rladlsxo1250@gmail.com>
2024-03-06 18:19:32 +0900

    알림 요청 기능 추가
    알림 요청기능은 targetSDK 버전 33이상부터 가능하기 때문에 버전도 올려줌

--- app/src/main/java/com/example/todo_list/activity/MainActivity.kt
+++ app/src/main/java/com/example/todo_list/activity/MainActivity.kt
@@ -1,12 +1,16 @@
 package com.example.todo_list.activity
 
+import android.Manifest
 import android.os.Bundle
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.app.AppCompatDelegate
 import androidx.navigation.findNavController
 import androidx.navigation.ui.setupWithNavController
 import com.example.todo_list.R
 import com.example.todo_list.databinding.ActivityMainBinding
+import com.gun0912.tedpermission.PermissionListener
+import com.gun0912.tedpermission.normal.TedPermission
 
 class MainActivity : AppCompatActivity() {
     lateinit var binding : ActivityMainBinding
@@ -17,6 +21,12 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        TedPermission.create()
+            .setPermissionListener(permission)
+            .setDeniedMessage("권한이 거부되었습니다. 설정 > 권한에서 허용해주세요.")
+            .setPermissions(Manifest.permission.POST_NOTIFICATIONS)
+            .check()
+
         binding.swiperefreshlayout.setOnRefreshListener {
             binding.swiperefreshlayout.isRefreshing = false
         }
@@ -26,5 +36,14 @@ class MainActivity : AppCompatActivity() {
 
         binding.navBar.setupWithNavController(navController)
 
+    }
+    private val permission = object : PermissionListener {
+        override fun onPermissionGranted() {
+            Toast.makeText(this@MainActivity, "권한 허가", Toast.LENGTH_SHORT).show()
+        }
+
+        override fun onPermissionDenied(deniedPermissions: MutableList<String>?) {
+            Toast.makeText(this@MainActivity, "권한 거부", Toast.LENGTH_SHORT).show()
+        }
     }
 }
