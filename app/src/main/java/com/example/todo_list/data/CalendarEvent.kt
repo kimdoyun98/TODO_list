b0f4483ae99733725747ef0774e034b325ee92d9
kimdoyun98 <rladlsxo1250@gmail.com>
2024-03-08 15:40:15 +0900

    Delete CalendarEvent.kt

--- app/src/main/java/com/example/todo_list/data/CalendarEvent.kt
+++
@@ -1,7 +0,0 @@
-package com.example.todo_list.data
-
-import java.time.LocalDate
-/**
- * com.kizitonwose.calendarview.CalendarView
- */
-data class CalendarEvent(val id: String, val text: String, val date: LocalDate)
