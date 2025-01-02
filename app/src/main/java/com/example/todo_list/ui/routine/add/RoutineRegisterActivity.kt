ce643767745286e40ad5ec70435c5120b4c78d4e
kimdoyun98 <rladlsxo1250@gmail.com>
2025-01-02 11:09:42 +0900

    [Refactor] 루틴 등록 시에 알림 권한 요청하도록 변경

--- app/src/main/java/com/example/todo_list/ui/routine/add/RoutineRegisterActivity.kt
+++ app/src/main/java/com/example/todo_list/ui/routine/add/RoutineRegisterActivity.kt
@@ -1,12 +1,18 @@
 package com.example.todo_list.ui.routine.add
 
+import android.Manifest
+import android.os.Build
 import android.os.Bundle
 import android.widget.CompoundButton
 import android.widget.TimePicker
+import android.widget.Toast
 import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
 import com.example.todo_list.R
 import com.example.todo_list.databinding.ActivityCycleRegisterBinding
+import com.example.todo_list.util.MyApplication
+import com.gun0912.tedpermission.PermissionListener
+import com.gun0912.tedpermission.normal.TedPermission
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
@@ -39,7 +45,30 @@ class RoutineRegisterActivity : AppCompatActivity(), TimePicker.OnTimeChangedLis
 
         binding.cycleRegister.setOnClickListener {
             viewModel.insert(binding.title.text.toString())
+            permission()
             finish()
+        }
+    }
+    
+    private fun permission() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
+            TedPermission.create()
+                .setPermissionListener(permission)
+                .setDeniedMessage(getString(R.string.permission_denied_message))
+                .setPermissions(Manifest.permission.POST_NOTIFICATIONS)
+                .check()
+        }
+    }
+
+    private val permission = object : PermissionListener {
+        override fun onPermissionGranted() {}
+
+        override fun onPermissionDenied(deniedPermissions: MutableList<String>?) {
+            Toast.makeText(
+                MyApplication.instance.applicationContext,
+                getString(R.string.permission_denied_toast_message),
+                Toast.LENGTH_SHORT
+            ).show()
         }
     }
 
